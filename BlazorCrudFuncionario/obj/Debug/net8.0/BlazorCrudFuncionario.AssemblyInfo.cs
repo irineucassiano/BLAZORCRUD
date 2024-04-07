23da32c5c9ee7a75d43d644e15277045ea738738
Irineu Cassinao <irineucassiano586@gmail.com>
2024-04-07 20:14:08 -0300

    Melhorias na parte de deletar funcionario

--- BlazorCrudFuncionario/obj/Debug/net8.0/BlazorCrudFuncionario.AssemblyInfo.cs
+++ BlazorCrudFuncionario/obj/Debug/net8.0/BlazorCrudFuncionario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCrudFuncionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74c249221aec6247c7c7b801136993b398b2e29")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCrudFuncionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCrudFuncionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
